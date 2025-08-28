82e124fae70dd728d3223694e01b32179a58fa23
Maooer <2847544066@qq.com>
2025-08-28 13:12:49 +0800

    开发MyInput输入框

--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -1,12 +1,40 @@
 <script setup lang="ts">
-
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'text',
+    enum: ['text', 'password', 'email'],
+    description: '输入框类型'
+  },
+  errorMessage: {
+    type: String,
+    default: '错误输入',
+    description: '错误提示'
+  },
+  minLength: {
+    type: Number,
+    default: 8,
+    description: '最小长度'
+  },
+  maxLength: {
+    type: Number,
+    default: 20,
+    description: '最大长度'
+  },
+  placeholder: {
+    type: String,
+    default: '请输入',
+    descriptioon: '占位文本'
+  }
+}) 
 </script>
 
 <template>
+  <div>
+    <input :type="type" :placeholder="placeholder" :maxlength="maxLength" :minlength="minLength">
+    <small>{{ props.errorMessage }}</small>
+  </div>
 
 </template>
 
-<style scoped lang="scss">
-
-</style>
-
+<style scoped lang="scss"></style>
